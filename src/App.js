9076604dbe132eb2e9a6e372a808a090ae680a12
subhadeep <subhadeepc19@gmail.com>
2021-05-24 14:23:07 +0530

    getting it into master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+    <script type="module" src="/main.js"></script>
+    <div id="head-card">
+      <div id="name-card">
+        <h1>Hi I am a DEVELOPER</h1> 
+      </div>
+      <div id="intro-card">
+        <p>I started devloping around a year ago and been doing this ever since</p>
+      </div>
+      <div id="my-socials">
+        <p>Github</p>
+        <p>Twitter</p>
+
+      </div>
     </div>
+    <div id="skill-card">
+      <div id="skill-head-1">
+        I have been using the following since some days
+      </div>
+      <div id="skill-list">
+      
+          <p>React Js</p>
+          <p>Vite Js(website made using this)</p>
+          <p>Node Js</p>
+          <p>Doge To the MOON</p>
+        
+      </div>
+      <div id="skill-head-2">
+        I am a big machine learning enthusiast:
+      </div>
+        <div id="skill-list">
+        
+            <p>Python</p>
+            <p>Linear Regression</p>
+            <p>Logisitic Regression</p>
+            <p>SVM</p>
+            <p>Clustering</p>
+            <p>PCA</p>
+            <p>LDA</p>
+            <p>t-SNE</p>
+            <p>Kaggle Novice</p>
+          
+        </div>
+    </div>
+    <div id="projects">
+      <div id="project-header">
+        These are all the projects
+      </div>
+      <div id="ml-header">These are all the ML projects</div>
+      <div id="projects-ML">
+          <p>Someting1</p>
+          <p>Someting2</p>
+          <p>Someting3</p>
+          <p>Someting4</p>
+      </div>
+      <div id="web-dev-header">These are all the web dev projects</div>
+      <div id="projects-WEB-DEV">
+          <p>Someting1</p>
+          <p>Someting2</p>
+          <p>Someting3</p>
+          <p>Someting4</p>
+      </div>
+    </div>
+    <div id="acheivements">
+      <div id="achieve-head">
+        These are all my acheivements
+      </div>
+      <div id="achieve-body">
+      
+          <p>Someting1</p>
+          <p>Someting2</p>
+          <p>Someting3</p>
+          <p>Someting4</p>
+        
+      </div>
+    </div>
+  </div>
   );
 }
 
